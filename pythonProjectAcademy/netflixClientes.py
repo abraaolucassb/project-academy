3d5d126c25b25740db55ac5a933f5df1e084cf1b
abraaolucassb <abraaolucassb@gmail.com>
2023-06-26 15:49:27 -0300

    add class

--- pythonProjectAcademy/netflixClientes.py
+++ pythonProjectAcademy/netflixClientes.py
@@ -16,7 +16,7 @@ class Client:
             print('Invalid plan!')
 
 
-    def ver_filme(self, movie, movie_plan):
+    def watch_movie(self, movie, movie_plan):
         if self.plan == movie_plan:
             print(f'Watch Movie: {movie}')
         elif self.plan == 'Premium':
@@ -29,9 +29,9 @@ class Client:
 
 client_test = Client('luke', 'luke@gmail.com', 'Basic')
 print(client_test.plan)
-client_test.ver_filme('Batman', 'Premium')
+client_test.watch_movie('Batman', 'Premium')
 
 
 client_test.change_plan('Premium')
 print(client_test.plan)
-client_test.ver_filme('Batman', 'Premium')
+client_test.watch_movie('Batman', 'Premium')
